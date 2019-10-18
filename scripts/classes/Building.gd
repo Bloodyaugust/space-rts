65ffd70fd2e55fad7c82dff68dfa6c697e72276d
Greyson Richey <grichey@mavenlink.com>
2019-10-18 17:25:27 -0600

    Make buildings spawn actors on ready
    - Test framework extended with a clear_scene function
    - Refinery now spawns 5 MiningDrone actors
    - Added empty MiningDrone actor
    
    Co-authored-by: Ethan Ransom <eransom@mavenlink.com>

--- scripts/classes/Building.gd
+++ scripts/classes/Building.gd
@@ -1,39 +1,65 @@
 extends Node2D
 class_name Building
 
+signal children_spawned
+
 export var id: String
+
+onready var root = get_tree().get_root()
 
 var inputs := []
 var output := {}
 var production_time: int
+var spawns := []
 
-#{
-#  "id": "refinery",
-#  "production": {
-#    "inputs": [
-#      {
-#        "id": "ore",
-#        "amount": 2
-#      }
-#    ],
-#    "output": {
-#      "id": "metal",
-#      "amount": 1
-#    },
-#    "time": 5
-#  }
-#}
+# {
+#   "id": "refinery",
+#   "spawns": [
+#     {
+#       "id": "mining-drone",
+#       "count": 5
+#     }
+#   ],
+#   "production": {
+#     "inputs": [
+#       {
+#         "id": "ore",
+#         "amount": 2
+#       }
+#     ],
+#     "output": {
+#       "id": "metal",
+#       "amount": 1
+#     },
+#     "time": 5
+#   }
+# }
 var _data := {}
 
 func _ready():
   _data = _load_building()
   _parse_data()
+  call_deferred("_spawn_children")
 
 func _parse_data():
 #  Eat the data into a first party data structure
   inputs = _data["production"]["inputs"]
   output = _data["production"]["output"]
   production_time = _data["production"]["time"]
+  spawns = _data["spawns"]
+
+func _spawn_children():
+  for spawn_definition in spawns:
+    var actor_packed_scene := load("res://actors/" + spawn_definition["id"] + ".tscn")
+    var new_actor
+
+    for i in range(spawn_definition["count"]):
+      new_actor = actor_packed_scene.instance()
+      new_actor.position = position
+
+      root.add_child(new_actor)
+
+  emit_signal("children_spawned")
 
 func _load_building():
   var file = File.new()
--- test/unit/Building.spec.gd
+++ test/unit/Building.spec.gd
@@ -1,17 +1,26 @@
 extends "res://addons/gut/test.gd"
 
+onready var tree := get_tree()
+
 var test_building : Building
 
 func before_each():
   test_building = Building.new()
-  test_building.id = "refinery"
+  test_building.id = "Refinery"
+  add_child(test_building)
   gut.p("ran setup", 2)
 
 func after_each():
+  clear_scene()
   gut.p("ran teardown", 2)
   
 func test_assert_building_loads():
-  test_building._ready()
   gut.p(test_building, 2)
+  yield(test_building, "children_spawned")
   assert_eq(test_building.inputs[0]["amount"], 2)
   assert_eq(test_building.inputs[0]["id"], "ore")
+  assert_eq(test_building.spawns[0]["count"], 5)
+
+func test_assert_spawns_actors():
+  yield(yield_for(0), YIELD)
+  assert_eq(tree.get_nodes_in_group("Drones").size(), 5)
